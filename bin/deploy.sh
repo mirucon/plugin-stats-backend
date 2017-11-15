13302d1ea6bc8fe99d52c4e45401ae1e901c7521
mirucon <i@miruc.co>
2017-11-15 22:26:10 +1300

    Update travis task

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+openssl version
+
 echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy.key\n" >> ~/.ssh/config
 openssl aes-256-cbc -k "$SERVER_KEY" -in .travis/deploy_key.enc -d -a -out deploy.key
 cp deploy.key ~/.ssh/
@@ -8,7 +10,6 @@ chmod 600 ~/.ssh/deploy.key
 
 git clone -b json --quiet "https://github.com/${TRAVIS_REPO_SLUG}.git"
 npm run build
-cd plugin-stats-backend
 git add plugins.min.json
 git commit -m "Update from travis $TRAVIS_COMMIT"
 git push origin json
