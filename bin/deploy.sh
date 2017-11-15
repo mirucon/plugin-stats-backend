e421d30629b7f37001c22e22176e0841457f471e
mirucon <i@miruc.co>
2017-11-15 21:20:49 +1300

    Update Travis task

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-git clone "https://github.com/${TRAVIS_REPO_SLUG}.git" json
+git clone -b json --quiet "https://github.com/${TRAVIS_REPO_SLUG}.git"
 npm run build
 git add plugins.min.json
 git commit -m "Update from travis $TRAVIS_COMMIT"
-git push -f --quiet "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" json 2> /dev/null
+git push "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" json
