111c6e9b7fb7be3544779c0ecf71f3e3ee88da86
mirucon <i@miruc.co>
2017-11-15 22:24:03 +1300

    Add deploy key

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy.key\n" >> ~/.ssh/config
+openssl aes-256-cbc -k "$SERVER_KEY" -in .travis/deploy_key.enc -d -a -out deploy.key
+cp deploy.key ~/.ssh/
+chmod 600 ~/.ssh/deploy.key
+
 git clone -b json --quiet "https://github.com/${TRAVIS_REPO_SLUG}.git"
 npm run build
+cd plugin-stats-backend
 git add plugins.min.json
 git commit -m "Update from travis $TRAVIS_COMMIT"
-git push "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" json
+git push origin json
