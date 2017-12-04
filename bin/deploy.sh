a88f5ceccfaca5021022127ba3afbfdc34343105
mirucon <i@miruc.co>
2017-12-04 22:57:17 +1300

    Fix build task

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -12,6 +12,7 @@ git clone -b json --quiet "https://github.com/${TRAVIS_REPO_SLUG}.git"
 npm run build
 mv plugins.min.json plugin-stats-backend
 cd plugin-stats-backend
-git add plugins.min.json
+git add plugins.json
+git rm plugins.min.json
 git commit -m "Update from travis $TRAVIS_COMMIT"
 git push --quiet "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" json 2> /dev/null
