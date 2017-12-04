a11ca9b578fc8b9c3442739423f06be0b5675f26
mirucon <i@miruc.co>
2017-12-05 08:17:57 +1300

    Remove unnecessary file

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -12,6 +12,6 @@ git clone -b json --quiet "https://github.com/${TRAVIS_REPO_SLUG}.git"
 mv plugins.json plugin-stats-backend
 cd plugin-stats-backend
 git add plugins.json
-git rm plugins.min.json
+git rm .DS_Store
 git commit -m "Update from travis $TRAVIS_COMMIT"
 git push --quiet "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" json 2> /dev/null
